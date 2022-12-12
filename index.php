8f718ba7108144d09602f05c04c4a1d8b989d97a
bernardDevDe <bernard14193@gmail.com>
2022-12-13 03:25:15 +0530

    side bar ui

--- index.php
+++ index.php
@@ -52,6 +52,10 @@
                 </svg>
                 </a>
             </div>
+            <div class="flex lg:hidden items-center lg:ml-auto">
+                <a type="button" href="./login.php" class="inline-block px-6 py-2.5 mr-2 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Login</a>
+                <a type="button" href="./register.php" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Sign up for free</a>
+            </div>
             <div class="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
                 <ul class="navbar-nav mr-auto lg:flex lg:flex-row">
                     <li class="nav-item pl-2">
@@ -68,10 +72,11 @@
                     </li>
                 </ul>
             </div>
-            <div class="flex items-center lg:ml-auto">
+            <div class="hidden lg:flex items-center lg:ml-auto ">
                 <a type="button" href="./login.php" class="inline-block px-6 py-2.5 mr-2 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Login</a>
                 <a type="button" href="./register.php" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Sign up for free</a>
             </div>
+            
         </div>
     </nav>
 
